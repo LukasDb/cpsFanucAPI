67764900c20c26253ddd9d3ab2bd77f9cebf3949
Elias Dellago <eliasdellago@gmail.com>
2021-11-02 15:18:15 +0100

    endpoint params

--- python/REST.py
+++ python/REST.py
@@ -1,6 +1,7 @@
 # Webserver
 import time
 
+import numpy as np
 from flask import Flask, Response
 from flask_restx import Resource, Api, reqparse
 import xml.etree.ElementTree as ET
@@ -49,8 +50,7 @@ class PickingPoint:
                   'w': str(self.w),
                   'p': str(self.p),
                   'r': str(self.r),
-                  PP_QUALITY_KEY: str(self.quality),
-                  }
+                  PP_QUALITY_KEY: str(self.quality)}
         ET.SubElement(parent, PP_KEY, params)
 
 
@@ -68,55 +68,38 @@ def serialize_picking_points_xml(picking_points: [PickingPoint]):
     return ET.tostring(root, encoding="ascii", method="xml")
 
 
-def degree_delta(alpha, beta):
+def calc_degree_delta(*, alpha, beta):
     delta = abs(alpha - beta)
     return delta if delta < 180 else 360 - delta
 
 
-def rotation_delta(current, target):
+def calc_rotation_delta(*, current, target):
     total_delta = 0
 
     # check if same dimensions
-    if len(current) != len(target):
+    if len(current) == len(target):
         # add all angle differences
-        for i in current:
-            total_delta += degree_delta(current[i], target[i])
+        for i in range(len(current)):
+            total_delta += calc_degree_delta(alpha=current[i], beta=target[i])
 
     return total_delta
-
-@api.route('/xml_endpoint')
-class XMLEndpoint(Resource):
-    def get(self):
-        # Parse arguments
-        parser = reqparse.RequestParser()
-        parser.add_argument('relative', type=bool)
-        args = parser.parse_args()
-
-        #time.sleep(0.2)
-        # Build response with flask function, default http code is 200 (OK)
-        # response = make_response({"text": text})
-        # Add header so Angular doesnt complain
-        # response.headers.add("Access-Control-Allow-Origin", "*")
-        # return response
-        root = ET.Element('list_tag')
-        for i in range(10):
-            x = ET.SubElement(root, 'struct_tag', {'int_val': str(i), 'float_val': str(-3.5 + i),
-                                                   'string_val': "abc" if i % 2 == 0 else "def"})
-
-        return Response(ET.tostring(root, encoding="ascii", method="xml"), mimetype='text/xml')
 
 
 # --------------------------------------------------------------------------
 
 @api.route('/objects')
 class ObjectsEndpoint(Resource):
+    # HTTP GET
     def get(self):
         # Parse arguments
         parser = reqparse.RequestParser()
-        parser.add_argument('obj-id', type=str)
+        # TODO if params needed
         args = parser.parse_args()
 
-        objects = [PickingObject([1.2, 3.4], 2.1) for i in range(5)]
+        pos_2d = np.array([2, 1])
+        distance = np.sqrt(np.square(pos_2d[0])+np.square(pos_2d[1]))
+
+        objects = [PickingObject(pos_2d, distance) for i in range(5)]
 
         return Response(serialize_picking_objects_xml(objects), mimetype='text/xml')
 
@@ -125,15 +108,38 @@ class ObjectsEndpoint(Resource):
 
 @api.route('/picking-points')
 class PickingPointsEndpoint(Resource):
+    # HTTP GET
     def get(self):
         # Parse arguments
         parser = reqparse.RequestParser()
-        parser.add_argument('relative', type=bool)
+        parser.add_argument('relative', type=int)
+        parser.add_argument('x', type=float)
+        parser.add_argument('y', type=float)
+        parser.add_argument('z', type=float)
+        parser.add_argument('w', type=float)
+        parser.add_argument('p', type=float)
+        parser.add_argument('r', type=float)
         args = parser.parse_args()
 
-        pp = [PickingPoint([4.2, 7.8, 2.3, 45, 90, 180], 2.1) for i in range(5)]
+        pose_6d = np.array([4.2, 7.8, 2.3, 45, 90, 180])
+        offset_6d = np.array([0, 0, 0, 0, 0, 0])
 
-        return Response(serialize_picking_points_xml(pp), mimetype='text/xml')
+        if args['relative']:
+            # if parameter relative is set, the 6d parameters are assumed to be present
+            offset_6d = np.array([
+                args['x'],
+                args['y'],
+                args['z'],
+                args['w'],
+                args['p'],
+                args['r']])
+
+        rotation_delta = calc_rotation_delta(current=pose_6d[3:], target=offset_6d[3:])
+        corrected_pose_6d = pose_6d - offset_6d
+
+        pps = [PickingPoint(corrected_pose_6d, rotation_delta) for i in range(5)]
+
+        return Response(serialize_picking_points_xml(pps), mimetype='text/xml')
 
 
 # --------------------------------------------------------------------------
@@ -141,6 +147,3 @@ class PickingPointsEndpoint(Resource):
 if __name__ == '__main__':
     # Host on 0.0.0.0 (all local addresses)
     app.run(host='0.0.0.0', port=5000, debug=False)
-
-
-
